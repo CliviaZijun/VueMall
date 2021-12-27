f281059e850746846e0be4c354b409f82d90c72b
Zijun Zhang <zhang_zijun@hotmail.com>
2021-12-28 02:04:32 +0800

    订单列表数据渲染

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -5,6 +5,49 @@
                 <span>请谨防钓鱼链接或诈骗电话，了解更多></span>
             </template>
         </order-header>
+        <div class="wrapper">
+            <div class="container">
+                <div class="order-box">
+                    <div class="order" v-for="(order,index) in list" :key="index">
+                        <div class="order-title">
+                            <div class="item-info fl">
+                                {{order.createTime}}
+                                <span>|</span>
+                                {{order.receiverName}}
+                                <span>|</span>
+                                订单号：{{order.orderNo}}
+                                <span>|</span>
+                                {{order.paymentTypeDesc}}
+                            </div>
+                            <div class="item-money fr">
+                                <span>应付金额：</span>
+                                <span class="money">{{order.payment}}</span>
+                                <span>元</span>
+                            </div>
+                        </div>
+                        <div class="order-content clearfix">
+                            <div class="goods-box fl">
+                                <div class="goods-list" v-for="(item,i) in order.orderItemVoList" :key="i">
+                                    <div class="goods-img">
+                                        <img v-lazy="item.productImage" alt="">
+                                    </div>
+                                    <div class="goods-name">
+                                        <div class="p-name">{{item.productName}}</div>
+                                        <div class="p-money">{{item.totalPrice}}元 × {{item.quantity}}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="goods-state fr" v-if="order.status == 20">
+                                <a href="javascript:;">{{order.statusDesc}}</a>
+                            </div>
+                            <div class="goods-state fr" v-else>
+                                <a href="javascript:;" @click="goPay(order.orderNo)">{{order.statusDesc}}</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -13,7 +56,104 @@ export default {
     name:'order-list',
     components:{
         OrderHeader
-    }
+    },
+    data(){
+        return{
+            list:[]
+        }
+    },
+    mounted(){
+        this.getOrderList();
+    },
+    methods:{
+        getOrderList(){
+            this.axios.get('/orders').then((res)=>{
+                this.list = res.list;
 
+            })
+        },
+        goPay(orderNo){
+            // 三种路由跳转方式
+            // this.$router.push('/order/pay');
+            this.$router.push({
+                path:'/order/pay',
+                query:{
+                    orderNo
+                }
+            })
+            /*this.$router.push({
+                name:'order-pay',
+                query:{
+                    orderNo
+                }
+            }) */
+        }
+    }
 }
 </script>
+<style lang="scss">
+    @import './../assets/scss/config.scss';
+    @import './../assets/scss/mixin.scss';
+    .order-list{
+        .wrapper{
+            background-color: $colorJ;
+            padding-top: 33px;
+            padding-bottom: 110px;
+            .order-box{
+                .order{
+                    @include border();
+                    background-color: $colorG;
+                    margin-bottom: 31px;
+                    &:last-child{
+                        margin-bottom: 0;
+                    }
+                    .order-title{
+                        @include height(74px);
+                        background-color: $colorK;
+                        padding: 0 43px;
+                        font-size: $fontI;
+                        color: $colorC;
+                        .item-info{
+                            span{
+                                margin: 0 9px;
+                            }
+                        }
+                        .money{
+                            font-size: $fontD;
+                            color: $colorB;
+                        }
+                    }
+                    .order-content{
+                        padding: 0 43px;
+                        .goods-box{
+                            width: 88%;
+                            .goods-list{
+                                display: flex;
+                                align-items: center;
+                                height: 145px;
+                                .goods-img{
+                                    width: 112px;
+                                    img{
+                                        width: 100%;
+                                    }
+                                }
+                                .goods-name{
+                                    font-size: $fontG;
+                                    color: $colorB;
+                                }
+                            }
+                        }
+                        .goods-state{
+                            @include height(145px);
+                            font-size: $fontG;
+                            color: $colorA;
+                            a{
+                                color: $colorA;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
